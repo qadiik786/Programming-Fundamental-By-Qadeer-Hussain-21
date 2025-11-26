2a8cc934c0212cee69b2666b466cbfa68a4056d7
qadiik786 <qadiik786@gmail.com>
2025-11-26 05:33:52 -0800

    Add files via upload

---
+++ program 01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"***********menu*************\n";
+	cout<<"1.\tBiryani\t\tRs.300\n";
+	cout<<"2.\tkarahi\t\tRs.700\n";
+	cout<<"3.\tQorma\t\tRs.400\n";
+	cout<<"4.\tdrinks\t\tRs.100\n";
+	return 0;
+}
